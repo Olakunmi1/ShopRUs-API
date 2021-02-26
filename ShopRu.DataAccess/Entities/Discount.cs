ccf10a3c315b98388c3c7f006d7548b0ee04fc0e
Olakunmi <agboolaolakunmi@gmail.com>
2021-02-27 00:56:17 +0100

    InitialModels

---
+++ ShopRu.DataAccess/Entities/Discount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopRUs_API.ShopRu.DataAccess.Entities
+{
+    public class Discount
+    {
+    }
+}
